0f02791e64c42378aef11143f7cf82cd58d07601
DamiaoCanndido <tower_gamer@hotmail.com>
2026-01-04 09:12:16 -0300

    Feat: Add field validations for UpdateUserDTO.

--- src/main/java/com/nergal/docseq/controllers/dto/UpdateUserDTO.java
+++ src/main/java/com/nergal/docseq/controllers/dto/UpdateUserDTO.java
@@ -4,12 +4,25 @@ import java.util.UUID;
 
 import com.nergal.docseq.controllers.validators.PasswordMatches;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+
 @PasswordMatches
 public record UpdateUserDTO(
+    @Min(value = 3, message = "Username must be at least 3 characters long")
     String username, 
-    String email, 
+
+    @Email(message = "Email should be valid")
+    String email,
+
     String role,
+
+    @Size(min = 6, message = "Password must have at least 6 characters")
     String password, 
+
+    @Size(min = 6, message = "Password must have at least 6 characters")
     String confirmPassword,
+
     UUID townshipId
 ) implements PasswordConfirmable { }
